397425606af94219fa73ccbb14dfcdec2c4d8232
Crisamer Ortega <crisamer.ortega@gmail.com>
2019-11-26 11:24:03 +0000

    js push

--- front/js/myjs.js
+++ front/js/myjs.js
@@ -1,3 +1,37 @@
+
+//SHOPPING CART code
+function loadShoppingCart(){
+
+    const JSONusers    = JSON.parse(localStorage.getItem('JSONusers'));
+    const JSONproducts = JSON.parse(localStorage.getItem('JSONproducts'));
+    const JSONcart     = JSON.parse(localStorage.getItem('JSONcart'));
+
+    var user;
+    $.each(JSONusers.users, function(index, element){
+        if(element.id == JSONcart.user_id){
+            user = element.name;
+        }
+    })
+    console.log(user);;
+//    document.getElementsByClassName('cart_title').appendChild(user);
+}
+
+function loadProducts(){
+
+    const JSONproducts = JSON.parse(localStorage.getItem('JSONproducts'));
+    var html;
+
+    $.each(JSONproducts.products, function(index, element){
+        html += '<div class="product"><img src="' + element.image + '" alt="' + element.name + '">'
+        '<div class="miniature">'+
+        '<a href="product.html" class="productLink"><p class="name">'+element.name+'</p>'
+        '<div class="info"> <p class="price">'+element.price+'</p> <button class="add">Add to cart</button></div></div></div>';
+    })
+
+    document.getElementById("productos").innerHTML = html;
+}
+
+
 function initialLoadJSONs(){
 
     const JSONusers = {'users':[
@@ -8,15 +42,15 @@ function initialLoadJSONs(){
     ]};
 
     const JSONproducts = {'products':[
-        {'product_id':1, 'seller_id':2, 'name':'art', 'price':[10, 27, 78], 'description':"art box",'suscription':[1, 3, 9], 'category':'art'},
-        {'product_id':2, 'seller_id':2, 'name':'knives', 'price':[10, 27, 78], 'description':'','suscription':[1, 3, 9], 'category':'survival'},
-        {'product_id':3, 'seller_id':3, 'name':'tools', 'price':[10, 27, 78], 'description':'','suscription':[1, 3, 9], 'category':'survival'},
-        {'product_id':4, 'seller_id':2, 'name':'Bio-cosmetics', 'price':[10, 27, 78], 'description':'','suscription':[1, 3, 9], 'category':'cosmetics'},
-        {'product_id':5, 'seller_id':3, 'name':'Maekup', 'price':[10, 27, 78], 'description':'','suscription':[1, 3, 9], 'category':'cosmetics'},
-        {'product_id':6, 'seller_id':3, 'name':'Korean food', 'price':[10, 27, 78], 'description':'','suscription':[1, 3, 9], 'category':'food'},
-        {'product_id':7, 'seller_id':2, 'name':'Japaneese food', 'price':[10, 27, 78], 'description':'','suscription':[1, 3, 9], 'category':'food'},
-        {'product_id':8, 'seller_id':3, 'name':'Nintendo merchandising', 'price':[10, 27, 78], 'description':'','suscription':[1, 3, 9], 'category':'freak'},
-        {'product_id':9, 'seller_id':2, 'name':'Random', 'price':[10, 27, 78], 'description':'','suscription':[1, 3, 9], 'category':'random'},
+        {'product_id':1, 'seller_id':2, 'name':'art', 'price':[10, 27, 78], 'description':"art box",'suscription':[1, 3, 9], 'category':'art', 'image':'../assets/products/art.jpg'},
+        {'product_id':2, 'seller_id':2, 'name':'knives', 'price':[10, 27, 78], 'description':'Box de cuchillos para el superviviente','suscription':[1, 3, 9], 'category':'survival', 'image':'../assets/products/survival.jpeg'},
+        {'product_id':3, 'seller_id':3, 'name':'tools', 'price':[10, 27, 78], 'description':'Box de cuchillos para el superviviente','suscription':[1, 3, 9], 'category':'survival', 'image':'../assets/products/survvival.jpeg'},
+        {'product_id':4, 'seller_id':2, 'name':'Bio-cosmetics', 'price':[10, 27, 78], 'description':'Box de cuchillos para el superviviente','suscription':[1, 3, 9], 'category':'cosmetics', 'image':'../assets/products/makeup.png'},
+        {'product_id':5, 'seller_id':3, 'name':'Maekup', 'price':[10, 27, 78], 'description':'Box de cuchillos para el superviviente','suscription':[1, 3, 9], 'category':'cosmetics', 'image':'../assets/products/makeup.png'},
+        {'product_id':6, 'seller_id':3, 'name':'Korean food', 'price':[10, 27, 78], 'description':'Box de cuchillos para el superviviente','suscription':[1, 3, 9], 'category':'food', 'image':'../assets/products/japFood.jpg'},
+        {'product_id':7, 'seller_id':2, 'name':'Japaneese food', 'price':[10, 27, 78], 'description':'Box de cuchillos para el superviviente','suscription':[1, 3, 9], 'category':'food', 'image':'../assets/products/japFood.jpg'},
+        {'product_id':8, 'seller_id':3, 'name':'Nintendo merchandising', 'price':[10, 27, 78], 'description':'Box de cuchillos para el superviviente','suscription':[1, 3, 9], 'category':'freak', 'image':'../assets/products/friki.jpeg'},
+        {'product_id':9, 'seller_id':2, 'name':'Random', 'price':[10, 27, 78], 'description':'Box de cuchillos para el superviviente','suscription':[1, 3, 9], 'category':'random', 'image':'../assets/products/random.png'},
     ]};
 
     const JSONcart = {
@@ -29,23 +63,8 @@ function initialLoadJSONs(){
     localStorage.setItem( 'JSONusers', JSON.stringify(JSONusers));
     localStorage.setItem( 'JSONproducts', JSON.stringify(JSONproducts));
     localStorage.setItem( 'JSONcart', JSON.stringify(JSONcart));
-}
 
-//SHOPPING CART code
-function loadShoppingCart(){
-
-    const JSONusers    = JSON.parse(localStorage.getItem('JSONusers'));
-    const JSONproducts = JSON.parse(localStorage.getItem('JSONproducts'));
-    const JSONcart     = JSON.parse(localStorage.getItem('JSONcart'));
-
-    var user
-    $.each(JSONusers.users, function(index, element){
-        if(element.id == JSONcart.user_id){
-            user = element.name;
-        }
-    })
-    console.log(user);;
-//    document.getElementsByClassName('cart_title').appendChild(user);
+    loadProducts();
 }
 
 /*******************************************************************************
